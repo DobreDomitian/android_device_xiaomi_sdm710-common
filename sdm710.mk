4d8dd1eb701d45ac5b466e09c57b2adbc359e156
Kraverix <domidoru@yahoo.com>
2021-06-14 00:36:48 +0300

    Import adapt and fix sepolicy for xiaomidoze & xiaomiparts

--- sdm710.mk
+++ sdm710.mk
@@ -26,6 +26,11 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts\
+    XiaomiCustomDoze
+    
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
